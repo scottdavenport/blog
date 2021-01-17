38587a4ddad501ed2d891a3a909a470093d11112
scottdavenport <scott@pinpoint.com>
2021-01-17 11:51:21 -0500

    Behind the Scenes of Redux Thunk - Better State

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = async () => {
+	return function (dispatch, getState) {
+		const promise = jsonPlaceholder.get('/posts');
+
+		return {
+			type: 'FETCH_POSTS',
+			payload: promise,
+		};
+	};
 	// Bad approach!!!
 	// We are breaking the rules of redux and an action creator
-	const response = await jsonPlaceholder.get('/posts');
-
-	return {
-		type: 'FETCH_POSTS',
-		payload: response,
-	};
 };
