56d7f2baef174e94fbc9b26d24484889842fa3f2
scottdavenport <scott@pinpoint.com>
2021-01-24 17:25:52 -0500

    Action Creators in Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,20 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch) => {
+	console.log('about to fetch posts!');
+	await dispatch(fetchPosts());
+	console.log('fetched posts!');
+};
+
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
 
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
